cce7420ca4f5cfd25ac174f130187844ed60ca74
ldimbe20 <ldimbe20@gmail.com>
2022-02-02 10:07:00 -0600

    Added data for all classes

--- gamerapi/models/url.py
+++ gamerapi/models/url.py
@@ -4,4 +4,4 @@ from django.db import models
 class URL(models.Model):
     playerId = models.ForeignKey("Player", on_delete=models.CASCADE)
     gameId = models.ForeignKey("Game", on_delete=models.CASCADE)
-    image_url=models.CharField(max_length=100)
+    image_url=models.CharField(max_length=300)
