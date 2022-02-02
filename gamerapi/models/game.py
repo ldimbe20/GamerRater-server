2876613ffe4ec54837d2fc9b15a833da2baf2cbd
ldimbe20 <ldimbe20@gmail.com>
2022-02-01 18:19:31 -0600

    added fixtures

--- gamerapi/models/game.py
+++ gamerapi/models/game.py
@@ -5,7 +5,7 @@ class Game(models.Model):
     designer = models.CharField(max_length=25)
     year_released = models.IntegerField()
     number_of_players = models.IntegerField()
-    game_duration = models.IntegerField()
+    game_duration = models.CharField(max_length=25)
     age_recommendation = models.IntegerField()
     title = models.CharField(max_length=50)
     
